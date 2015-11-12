9e4e559c559c1b2f335ce4174d78916406a693f2
Miller Augusto <miller.augusto@gmail.com>
2015-11-12 16:36:38 -0200

    Update img-preload.js

--- dist/img-preload.js
+++ dist/img-preload.js
@@ -5,29 +5,31 @@
  * Licensed under the MIT license.
  * http://opensource.org/licenses/MIT
  */
-$.imgPreload = function(config, callback){
-    if(config){
-        $(config.loading).fadeIn('slow');
-
-        var div = $('<div/>');
-        $('body').append(div);
-
-        for(var key in config.list){
-            $(div).append($('<img/>').attr('src', config.list[key][config.attr]));
-        }
-
-        if(callback){
-            setTimeout(function(){
-                $(div).remove();
-                $(config.loading).fadeOut('slow');
-                callback();
-            }, 2000);
-        }
-
-        return {
-            clear: function(){
-                $(div).remove();
+(function($) {
+    $.imgPreload = function(config, callback){
+        if(config){
+            $(config.loading).fadeIn('slow');
+    
+            var div = $('<div/>');
+            $('body').append(div);
+    
+            for(var key in config.list){
+                $(div).append($('<img/>').attr('src', config.list[key][config.attr]));
             }
-        };
-    }
-};
+    
+            if(callback){
+                setTimeout(function(){
+                    $(div).remove();
+                    $(config.loading).fadeOut('slow');
+                    callback();
+                }, 2000);
+            }
+    
+            return {
+                clear: function(){
+                    $(div).remove();
+                }
+            };
+        }
+    };
+}(jQuery));
